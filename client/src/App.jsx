11267fff751fe6b36c4f8676144b4496eb8d4442
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-02 16:39:03 +0200

    added nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import NavigationSection from './components/NavigationSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavigationSection />
     </>
   )
 }
---
+++ client/src/components/NavigationSection.jsx
@@ -0,0 +1,7 @@
+export default function NavigationSection() {
+    return(
+        <nav className="nav">
+
+        </nav>
+    )
+}
