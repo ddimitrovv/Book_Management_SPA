0130218273e63b124c70db635c1f37d81175a9b9
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-02 14:33:16 +0200

    added function for singnals in UserConfig

--- server/server/users/apps.py
+++ server/server/users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'server.users'
+
+    def ready(self):
+        import server.users.signals
