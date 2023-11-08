686f90c6ae264ddf50c747bb185b74aba53f6934
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-08 14:02:27 +0200

    added endpoint for token

--- server/server/users/urls.py
+++ server/server/users/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from server.users.views import user_registration, user_login, user_logout
+from server.users.views import get_csrf_token, user_registration, user_login, user_logout
 
 urlpatterns = [
+    path('token/', get_csrf_token, name='get_csrf_token'),
     path('register/', user_registration, name='user_registration'),
     path('login/', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
--- server/server/users/views.py
+++ server/server/users/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth import login, authenticate, logout
+from django.views.decorators.csrf import get_token
 
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
 
 from server.users.models import CustomUser
 from server.users.serializers import UserRegistrationSerializer, UserLoginSerializer, CustomUserSerializer
+
+
+@api_view(['GET'])
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return Response({'csrf_token': csrf_token})
 
 
 @api_view(['POST'])
@@ -55,16 +62,16 @@ def user_login(request):
             if user is not None:
                 login(request, user)  # Log in the user
                 token, _ = Token.objects.get_or_create(user=user)
-                return Response({
-                    'user': CustomUserSerializer(user).data,
-                    'token': token.key
-                }, status=status.HTTP_200_OK)
+                return Response(
+                    {'user': CustomUserSerializer(user).data, 'token': token.key},
+                    status=status.HTTP_200_OK
+                )
             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
-@authentication_classes([TokenAuthentication])
+@authentication_classes([SessionAuthentication, BasicAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def user_logout(request):
     if request.method == 'POST':
